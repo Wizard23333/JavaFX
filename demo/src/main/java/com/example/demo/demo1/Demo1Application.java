31b027d4baaa5230cc026d4c9b26d73ed53e4d91
haha <2210946752@qq.com>
2021-10-19 22:51:46 +0800

    Update Demo1Application.java

--- demo/src/main/java/com/example/demo/demo1/Demo1Application.java
+++ demo/src/main/java/com/example/demo/demo1/Demo1Application.java
@@ -3,6 +3,10 @@ package com.example.demo.demo1;
 import com.example.demo.MainWindowApplication;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +16,14 @@ public class Demo1Application extends Stage {
         FXMLLoader fxmlLoader = new FXMLLoader(MainWindowApplication.class.getResource("demo1-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 480, 310);
 
+        Label label1 = new Label("I'm a label created by code");
+        label1.setFont(new Font("Arial", 30));
+        label1.setTextFill(Color.web("#0076a3"));
+
         // 在这里修改每个演示的内容
         // 添加各种的功能演示
 
+        //scene.
         this.setScene(scene);
     }
 }
